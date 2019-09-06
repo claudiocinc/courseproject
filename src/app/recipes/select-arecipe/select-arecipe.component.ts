3dda67a7ca8c9ec9ee02e276e0def8b7d4a1ea34
claudiocinc <54982760+claudiocinc@users.noreply.github.com>
2019-09-06 12:22:12 +0200

    Start

---
+++ src/app/recipes/select-arecipe/select-arecipe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-arecipe',
+  templateUrl: './select-arecipe.component.html',
+  styleUrls: ['./select-arecipe.component.css']
+})
+export class SelectARecipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
